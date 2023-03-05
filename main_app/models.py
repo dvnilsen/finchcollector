1670cb5bcf432d23f7843a991f1e758d8c7da34d
dvnilsen <dvnilsen@protonmail.com>
2023-03-05 12:35:48 -0800

    reverted back to part 4

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.urls import reverse 
 
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+##### START -----> 
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   color = models.CharField(max_length=100)
@@ -15,3 +23,20 @@ class Finch(models.Model):
   
   def get_absolute_url(self):
     return reverse("detail", kwargs={"finch_id": self.id})
+##### END
+
+##### START ----->
+class Feeding(models.Model):
+  date = models.DateField("Feeding Date")
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
